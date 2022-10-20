90b77ff2e7ef9e1b4b6c1518e0695ba01f40c372
Jamie Mac <macster110@gmail.com>
2022-10-20 09:27:07 +0100

    Initial Commit

---
+++ X3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  X3
+//
+//  Created by Jamie Donald John Macaulay on 20/10/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
